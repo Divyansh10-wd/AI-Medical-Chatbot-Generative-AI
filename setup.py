1c7a96eef6c47db8e2e95b5566bd8ed0e70773f9
Divyansh10-wd <divyanshmadan1010@gmail.com>
2025-06-25 14:30:13 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Divyansh',
+    author_email='divyanshmadan1010@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
